591c9c6eab79df954a536be9506e2953ad5559f6
alifianadexe <alifianadexe@gmail.com>
2018-11-24 14:18:28 +0700

    Finishing : a build

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Email: alifianadexe@gmail.com 
+            <br />
+           Phone: 083838203528
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
